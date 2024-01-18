a61c8dc7ca0632264f182861b5eec663a300555d
touhid1 <touhidulislam066.com>
2024-01-18 13:46:39 +0600

    WebPageOpenClose

---
+++ src/test/java/com/touhedul/DriverSetup.java
@@ -0,0 +1,20 @@
+package com.touhedul;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class DriverSetup {
+    public WebDriver browser;
+
+    @BeforeSuite
+    public void openABrowser(){
+        browser = new ChromeDriver();
+        browser.manage().window().maximize();
+    }
+
+    @AfterSuite
+    public void quiteBrowser(){
+        browser.quit();
+    }
+}
